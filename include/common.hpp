e474e2f1e1ff8a3dd2477986bdcc35b6a940b609
linxichen <linxichen88@gmail.com>
2015-08-03 16:18:33 -0400

    first seemmingly working code.

--- include/common.hpp
+++ include/common.hpp
@@ -1,6 +1,9 @@
 #ifndef COMMONHEADER
 #define COMMONHEADER
 
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
 /// Header file that contains things many files needs to know
 
 /// typedef useful things
@@ -12,51 +15,32 @@ typedef thrust::device_vector<int>    d_vec_i;
 // A struct that encompasses host and device vectors
 template<class T>
 struct cudavec {
-	// data members contains host and device vectors
+	/// data members contains host and device vectors
 	size_t n;
 	thrust::host_vector<T> hvec;
 	thrust::device_vector<T> dvec;
 	T* hptr;
 	T* dptr;
 
-	// constructor, with random default value
-	cudavec(size_t _n) {
-		n = _n;
-		hvec = thrust::host_vector<T>(n);
-		dvec = thrust::device_vector<T>(n);
-		hptr = thrust::raw_pointer_cast(hvec.data());
-		dptr = thrust::raw_pointer_cast(dvec.data());
-	};
+	/// constructor, with random default value of size
+	cudavec(size_t);
 
-	// constructor, with specified value x
-	cudavec(size_t _n, T x) {
-		n = _n;
-		hvec = thrust::host_vector<T>(n,x);
-		dvec = thrust::device_vector<T>(n,x);
-		hptr = thrust::raw_pointer_cast(hvec.data());
-		dptr = thrust::raw_pointer_cast(dvec.data());
-	};
+	/// constructor, with specified value x
+	cudavec(size_t _n, T x);
 
 	// get size
-	size_t size() {
-		return n;
-	};
+	size_t size();
 
-	// copy from host to device
-	void h2d() {
-		dvec = hvec;
-	};
+	/// copy from host to device
+	void h2d();
 
-	// copy from device to host
-	void d2h() {
-		hvec = dvec;
-	};
+	/// copy from device to host
+	void d2h();
 
-	// on the host, wrap the [] operator, you can directly write
-	// the host_vector. pretty bad practice though.
-	__host__
-	T& operator[] (const int i){
-		return hptr[i];
-	};
+	///
+	/// on the host, wrap the [] operator, you can directly write
+	/// the host_vector. pretty bad practice though. */
+	///
+	T& operator[] (const unsigned int );
 };
 #endif
